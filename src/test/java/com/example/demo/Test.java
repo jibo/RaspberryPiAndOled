f23819945d66cc7d8730b0e66bdb2293ae48afae
jibo <guzhibobogu@gmail.com>
2023-08-02 15:59:14 +0800

    已修改

--- src/test/java/com/example/demo/Test.java
+++ src/test/java/com/example/demo/Test.java
@@ -11,7 +11,6 @@ import org.bytedeco.javacv.Java2DFrameConverter;
 //参考原文链接：https://blog.csdn.net/lazy_p/article/details/7165999
 public class Test {
     public static void binaryImage(BufferedImage image1,int k) throws IOException{
-//        File file = new File("D:\\MinecraftModProject\\picture\\12.jpg");
         BufferedImage image = image1;
 
         int width = image.getWidth();
@@ -24,30 +23,27 @@ public class Test {
                 grayImage.setRGB(i, j, rgb);
             }
         }
-
-//        File newFile = new File("D:\\MinecraftModProject\\picture\\a.jpg");
         ImageIO.write(grayImage, "jpg", new File("D:\\MinecraftModProject\\picture\\" + k + ".jpg"));
-//        ImageIO.write(grayImage, "jpg", newFile);
     }
 
-//    public static void grayImage() throws IOException{
-//        File file = new File("D:\\MinecraftModProject\\picture\\12.jpg");
-//        BufferedImage image = ImageIO.read(file);
-//
-//        int width = image.getWidth();
-//        int height = image.getHeight();
-//
-//        BufferedImage grayImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);//重点，技巧在这个参数BufferedImage.TYPE_BYTE_GRAY
-//        for(int i= 0 ; i < width ; i++){
-//            for(int j = 0 ; j < height; j++){
-//                int rgb = image.getRGB(i, j);
-//                grayImage.setRGB(i, j, rgb);
-//            }
-//        }
-//
-//        File newFile = new File("D:\\MinecraftModProject\\picture\\b.jpg");
-//        ImageIO.write(grayImage, "jpg", newFile);
-//    }
+    public static void grayImage() throws IOException{
+        File file = new File("D:\\MinecraftModProject\\picture\\12.jpg");
+        BufferedImage image = ImageIO.read(file);
+
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        BufferedImage grayImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+        for(int i= 0 ; i < width ; i++){
+            for(int j = 0 ; j < height; j++){
+                int rgb = image.getRGB(i, j);
+                grayImage.setRGB(i, j, rgb);
+            }
+        }
+
+        File newFile = new File("D:\\MinecraftModProject\\picture\\b.jpg");
+        ImageIO.write(grayImage, "jpg", newFile);
+    }
     public static void main(String[] args) throws IOException {
         String video = "D:\\MinecraftModProject\\1.mp4";
 //        总4590帧图片    帧率*时长
